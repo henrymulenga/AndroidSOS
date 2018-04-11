5d8b9b13a79db49f285394ec7f0c1755e8eeb617
Masitano <masitano.sichone@gmail.com>
2018-04-11 11:07:10 +0200

    Test Commit

--- app/src/main/java/com/henrymulenga/androidsos/LoginActivity.java
+++ app/src/main/java/com/henrymulenga/androidsos/LoginActivity.java
@@ -56,6 +56,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
+    private static final int REQUEST_READ_NOTHING = 1;
 
     /**
      * A dummy authentication store containing known user names and passwords.
